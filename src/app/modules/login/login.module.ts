99d7790297ab43a3b86bfddb12e1ae5189849403
Uniliva <uniliva.pereira@yaman.com.br>
2020-03-04 18:46:49 -0300

    Adicionando tela de login

---
+++ src/app/modules/login/login.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from 'src/app/material/material.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MaterialModule
+  ]
+})
+export class LoginModule { }
